0303e33c03f68ed99956208e69934b60085cd7a3
alvaroaraujo1 <alvaro.0410410@gmail.com>
2021-04-02 17:29:46 -0500

    Updated

---
+++ TP_Experimentos/src/main/resources/data.sql
@@ -0,0 +1,31 @@
+
+
+INSERT INTO role (name_role)	VALUES ('ROLE_ADMIN');
+
+INSERT INTO account(last_name_account, name_account, password_account, user_account, id_role) VALUES ('Araujo', 'Alvaro', '$2a$10$gZa9tPtmx0ez.mFywobleet3Xc4d/6irYNibu2Y47zNyIYIwkwvKq', 'alvaro', 1);
+
+INSERT INTO Students(id_student, date_of_admission_student, date_of_birth_student, lastname_student, name_student) VALUES (201713444, '2017-03-27', '2000-10-04', 'Araujo Arevalo', 'Alvaro Sebastian');
+INSERT INTO Students(id_student, date_of_admission_student, date_of_birth_student, lastname_student, name_student) VALUES (201612953, '2015-11-18', '1999-01-09', 'Amezquita Mendoza', 'Carlos Alfredo Tomas');
+INSERT INTO Students(id_student, date_of_admission_student, date_of_birth_student, lastname_student, name_student) VALUES (201814238, '2017-10-17', '2001-05-04', 'Arroyo Camacho', 'Mario Piero');
+INSERT INTO Students(id_student, date_of_admission_student, date_of_birth_student, lastname_student, name_student) VALUES (201719761, '2016-10-05', '2000-07-26', 'Benancio Mauricio', 'Sergio Leonardo');
+INSERT INTO Students(id_student, date_of_admission_student, date_of_birth_student, lastname_student, name_student) VALUES (201414238, '2017-10-17', '2001-05-04', 'Landa Valencia', 'Renzo Enrique');
+INSERT INTO Students(id_student, date_of_admission_student, date_of_birth_student, lastname_student, name_student) VALUES (201519761, '2016-10-05', '2000-07-26', 'Matos Gavino', 'Carlos Danny');
+
+INSERT INTO Courses (name_course) VALUES ('Diseño de Experimentos');
+INSERT INTO Courses (name_course) VALUES ('Emprendimiento Negocios Sostenibles');
+INSERT INTO Courses (name_course) VALUES ('Fundamentos Seguridad Auditoría');
+INSERT INTO Courses (name_course) VALUES ('Implementación Sistemas Empresariales');
+INSERT INTO Courses (name_course) VALUES ('Soluciones Móviles Cloud');
+INSERT INTO Courses (name_course) VALUES ('Fundamentos Inteligencia Negocios');
+INSERT INTO Courses (name_course) VALUES ('Gerencia de Proyectos');
+INSERT INTO Courses (name_course) VALUES ('Seminario II');
+INSERT INTO Courses (name_course) VALUES ('Taller Desempeño Profesional');
+
+INSERT INTO Teachers(id_teacher, date_of_admission_teacher, date_of_birth_teacher, lastname_teacher, name_teacher) VALUES (09203897, '2007-07-20', '1970-01-09', 'Cerda Garcia', 'Ruben Oscar');
+INSERT INTO Teachers(id_teacher, date_of_admission_teacher, date_of_birth_teacher, lastname_teacher, name_teacher) VALUES (98765432, '2009-09-10', '1972-09-20', 'Davila Orozco', 'Cesar Antonio');
+INSERT INTO Teachers(id_teacher, date_of_admission_teacher, date_of_birth_teacher, lastname_teacher, name_teacher) VALUES (74158963, '2002-11-18', '1965-04-01', 'Farje Tong', 'Alfonso Raul');
+INSERT INTO Teachers(id_teacher, date_of_admission_teacher, date_of_birth_teacher, lastname_teacher, name_teacher) VALUES (95487632, '2004-10-14', '1968-03-28', 'Mansilla Lopez', 'Juan Pablo');
+INSERT INTO Teachers(id_teacher, date_of_admission_teacher, date_of_birth_teacher, lastname_teacher, name_teacher) VALUES (85274196, '2008-11-14', '1970-08-27', 'Herrera Trujillo', 'Emilio Arturo');
+INSERT INTO Teachers(id_teacher, date_of_admission_teacher, date_of_birth_teacher, lastname_teacher, name_teacher) VALUES (36251497, '2002-12-05', '1970-05-13', 'Chinchay Celada', 'Milton Enrique');
+
+
